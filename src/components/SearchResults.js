ea024519d4ad43bb3c6efbc02d5f3e6bff0ca6c8
Bill Early <earlybilly@gmail.com>
2019-02-12 20:38:12 -0500

    Radio buttons instead of unordered list

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledList = styled.ul`
+const StyledList = styled.div`
   background-color: white;
   border-radius: 0.25rem;
   box-shadow: 0 0.15em 1.5em #00000036;
   margin: 1rem 0 0;
+  overflow: hidden;
   padding: 0;
 `;
 
-const StyledResult = styled.li`
+const StyledResult = styled.div`
   border-bottom: 0.05rem solid lightgray;
-  list-style-type: none;
-  margin: 0 0.5rem;
-  padding: 0.5rem 0;
 
   &:last-child {
     border: none;
   }
 `;
 
+const StyledInput = styled.input`
+  opacity: 0;
+  position: fixed;
+
+  &:checked+label {
+    background-color: #e68181;
+  }
+`;
+
+const StyledLabel = styled.label`
+  cursor: pointer;
+  display: block;
+  padding: 0.5rem;
+`;
+
 export const SearchResults = ({ results }) => {
   const mapResults = () => {
-    return results.map(result =>
-      <StyledResult>{result.label}</StyledResult>
+    return results.map((result, i) =>
+      <StyledResult>
+        <StyledInput type="radio" name="result" id={`result-${i}`} value={result.label} />
+        <StyledLabel for={`result-${i}`}>{result.label}</StyledLabel>
+      </StyledResult>
     );
   };
 
